db2c6d42ebe8c0161c5a554d4671ea01c9dae150
h4qan <hakkanero@gmail.com>
2023-11-30 23:06:21 -0500

    Update project.py

--- project.py
+++ project.py
@@ -27,7 +27,8 @@ class Resource:
 		self.utilization_rate = 0
 		self.remaining = 1
 
-	def utilize(self, agent_id, amount):
+	def utilize(self, agent, amount):
+		agent_id = agent.id
 		if agent_id in self.utilizers:
 			self.utilizers[agent_id] += amount
 		else:
@@ -43,6 +44,9 @@ class Resource:
 		pyplot.figure(self.name + "1" if unique else self.name)
 		pyplot.pie(amounts, labels=labels)
 	
+	def get_utilization(self, agent):
+		return self.utilizers[agent.id]
+
 	def __str__(self):
 		return self.name
 
@@ -73,6 +77,9 @@ class Group:
 				current_agents.append(a)
 		return current_agents
 
+	def length(self):
+		return len(self.agents)
+
 class Algorithm:
 	'''Algorithm class, represents the base of a resource allocation algorithm'''
 	nor : int #number_of_resources
@@ -90,7 +97,7 @@ class Algorithm:
 		#initially, give everyone 1/n of their demand
 		for a in self.agents:
 			for j,d in enumerate(a.demand_vec):
-				self.resources[j].utilize(a.id,d/self.n)
+				self.resources[j].utilize(a,d/self.n)
 
 	def group_agents(self, verbose : bool = False):
 		self.groups = [Group(i) for i in range(self.nor)]
@@ -108,9 +115,19 @@ class Algorithm:
 		for i in range(self.nor):
 			g = self.groups[i]
 			if g.total_value() < current_min_value:
+				current_min_value = g.length()
+				current_group = g
+		return current_group
+		"""
+		current_group = None
+		current_min_value = 1000000000.0 #arbitrarily large number
+		for i in range(self.nor):
+			g = self.groups[i]
+			if g.total_value() < current_min_value:
 				current_min_value = g.total_value()
 				current_group = g
 		return current_group
+		"""
 
 	def print_res(self):
 		for r in self.resources:
@@ -124,77 +141,105 @@ class UNB(Algorithm):
 	'''UNB algorithm stands for UNBalanced. When groups are unbalanced, only increase the share of the agent with lowest share in the smallest group.'''
 	def __init__(self, number_of_resources=2, number_of_agents=3) -> None:
 		super().__init__(number_of_resources, number_of_agents)
-		self.share_function()
 
 	def share_function(self):
 		super().share_function() #Give everyone 1/n of their demand to satisfy SI.
 		super().group_agents() #Group agents
-		self.lesser = self.lowest_group() #which group is the lesser one (0 for group 1, 1 for group 2)
-		self.smallestFrac = float("inf") #the smallest fraction of a resource given to an agent 
+		#self.lesser = self.lowest_group() #which group is the lesser one (0 for group 1, 1 for group 2)
 
 	#UNB algorithim when group 1 is bigger
 	def step2group1(self):
 		'''when group 1 is bigger we look at group 2 and give resource 1 to the smallest agents of group 2'''
-		while self.resources[0].remaining > 0 and self.resources[1].remaining > 0:
-			
-			#find the agents with the smallest fraction share of resource 1 in group 2
-			
-			#need to find new smallest agent each iteration
-			P = self.lesser.smallest_agents_of_resource(1)
-			
-			self.smallestFrac = self.lesser.utilizers[smallestAgent]
-			p = []
-			for agent in self.lesser.agents:
-				if self.resources[0].utilizers[agent] == self.smallestFrac:
-					p.append(agent)
-			
-			#calculate the rates
-			
-			#this one we find the smallest agent of resource 1 thats not in P (basically the second lowest frac) minus the smallest frac in P
-			n_subP = []
-			for agent in self.agents:
-				if agent in p:
-					continue
-				else:
-					n_subP.append(agent)
-			minAgent = float("inf") #smallest agent outside of P
-			for agent in n_subP:
-				if self.resources[0].utilizers[agent] < minAgent:
-					minAgent = agent 
-			s0 = self.resources[0].utilizers[agent] - self.smallestFrac
-			s1 = self.resources[0].remaining / len(p) 
-			#denominator for s2
-			s2_denom = 0
-			for j in p:
-				cur = 1 / j.demand_vec[0]
-				s2_denom += cur
-			s2 = self.resources[1].remaining / s2_denom
-			s = min(s0,s1,s2)
-
-			for agent in p:
-				#increase agents share by some rate
-				self.resources[give].utilizers[agent] += s
-				#decrement overall resource by the rate 
-				self.resources[0].remaining -= s
-				self.resources[1].remaining -= (s/self.resources[0].utilizers[agent])
-			
+		#find the agents with the smallest fraction share of resource 1 in group 2
+		lesser = self.lowest_group()
+		#need to find new smallest agent each iteration
+		P = lesser.smallest_agents_of_resource(1)
+		for a in P:
+			print(a)
+		
+		#self.smallestFrac = self.lesser.utilizers[smallestAgent]
+		#p = []
+		#for agent in self.lesser.agents:
+		#	if self.resources[0].utilizers[agent] == self.smallestFrac:
+		#		p.append(agent)
+		
+		#calculate the rates
+		
+		#this one we find the smallest agent of resource 1 thats not in P (basically the second lowest frac) minus the smallest frac in P
+		n_subP = [a for a in self.agents if a not in P]
+	
+		minAgentVal = float("inf") #smallest agent outside of P
+		for agent in n_subP:
+			if self.resources[0].get_utilization(agent) < minAgentVal:
+				minAgentVal = self.resources[0].get_utilization(agent)
+		s0 = minAgentVal - self.resources[0].get_utilization(P[0])
+		s1 = self.resources[0].remaining / len(P) 
+		#denominator for s2
+		s2_denom = 0
+		for j in P:
+			cur = 1.0 / j.demand_vec[0]
+			s2_denom += cur
+		s2 = self.resources[1].remaining / s2_denom	
+		print(s0,s1,s2)
+		s = min(s0,s1,s2)
+		for agent in P:
+			#increase agents share by some rate
+			self.resources[0].utilize(agent, s)
+			self.resources[1].utilize(agent, s/agent.get_demand(0))
 		return self.agents
 	
 	#the UNB algorithim when group 2 is bigger basically the same method as above but switch the numbers 
-	def step2group2():
-		pass
+	def step2group2(self):
+		#find the agents with the smallest fraction share of resource 1 in group 2
+		lesser = self.lowest_group()
+		#need to find new smallest agent each iteration
+		P = lesser.smallest_agents_of_resource(0)
+		for a in P:
+			print(a)
+		
+		#self.smallestFrac = self.lesser.utilizers[smallestAgent]
+		#p = []
+		#for agent in self.lesser.agents:
+		#	if self.resources[0].utilizers[agent] == self.smallestFrac:
+		#		p.append(agent)
+		
+		#calculate the rates
+		
+		#this one we find the smallest agent of resource 1 thats not in P (basically the second lowest frac) minus the smallest frac in P
+		n_subP = [a for a in self.agents if a not in P]
+		
+		minAgentVal = float("inf") #smallest agent outside of P
+		for agent in n_subP:
+			if self.resources[1].get_utilization(agent) < minAgentVal:
+				minAgentVal = self.resources[1].get_utilization(agent)
+		s0 = minAgentVal - self.resources[1].get_utilization(P[0])
+		s1 = self.resources[1].remaining / len(P) 
+		#denominator for s2
+		s2_denom = 0
+		for j in P:
+			cur = 1.0 / j.demand_vec[1]
+			s2_denom += cur
+		s2 = self.resources[1].remaining / s2_denom
+		print(s0,s1,s2)
+		s = min(s0,s1,s2)
+		for agent in P:
+			#increase agents share by some rate
+			self.resources[1].utilize(agent, s)
+			self.resources[0].utilize(agent, s/agent.get_demand(1))
+		return self.agents
+		
 	
 	def share_function(self):
 		super().share_function() #Give everyone 1/n of their demand to satisfy EF.
 		super().group_agents() #Group the agents based on their most demanded resource.
 		
 	def process(self):
-		lowest_group = self.lowest_group()
-		smallest_agent = lowest_group.smallest_agent()
-
-		increase_amount = 0.1 # change this to second smallest - smallest
-
-		self.resources[lowest_group.resource_num].utilize(smallest_agent.id, increase_amount)
+		for i in range(10):#while self.resources[0].remaining > 0 and self.resources[1].remaining > 0.2:
+			#if group1 bigger
+			if self.groups[0].length() > self.groups[1].length():
+				self.step2group1()
+			else:
+				self.step2group2()
 
 
 class BAL(Algorithm):
@@ -290,12 +335,18 @@ def unb_test():
 	alg = UNB()
 	alg.resources.append(Resource("CPU"))
 	alg.resources.append(Resource("Memory"))
-	alg.agents.append(Agent("A", 0.1,0.7))
-	alg.agents.append(Agent("B", 1.0,0.3))
-	alg.agents.append(Agent("C", 0.2,0.4))
+	alg.agents.append(Agent("A", 0.2,0.3))
+	alg.agents.append(Agent("B", 0.4,0.1))
+	alg.agents.append(Agent("C", 0.4,0.2))
 	alg.share_function()
 	alg.show_resources()
+	for r in alg.resources:
+		for i in r.utilizers:
+			print("resource",r,"agent",i,r.utilizers[i])
 	alg.process()
+	for r in alg.resources:
+		for i in r.utilizers:
+			print("resource",r,"agent",i,r.utilizers[i])
 	alg.show_resources(True)
 	pyplot.show()
 
